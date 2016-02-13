0e9b9095204d5f0fe02e2bca0d077e496bc29360
Shidhin C R <shidhincr@gmail.com>
2016-02-13 14:05:01 +0400

    rebuilding site Sat Feb 13 14:05:01 GST 2016

--- publish.sh
+++ publish.sh
@@ -28,7 +28,7 @@ echo -e "\033[0;32mPushing changes to Github...\033[0m"
 # git push origin master
 # Commit changes.
 
-g push origin source
+git push origin source
 
 echo -e "\033[0;32mDeploying updates to Github...\033[0m"
 
