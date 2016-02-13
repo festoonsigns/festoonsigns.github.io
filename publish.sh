6c7a156b86dfb2f2e3bce8d60a3acad6777104d9
Shidhin C R <shidhincr@gmail.com>
2016-02-13 13:13:29 +0400

    Initial commit  * keeping the empty folders

---
+++ publish.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Make sure this script is run alongside the hugo content
+# even if run as an executable.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+echo -e "\033[0;32mBuilding project...\033[0m"
+
+# Build the project.
+hugo -s ./
+
+echo -e "\033[0;32mCommitting changes...\033[0m"
+
+# Add changes to git.
+git add --all .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+echo -e "\033[0;32mDeploying updates to Github...\033[0m"
+
+# Push source and build repos.
+# git push origin master
+
+echo -e "\033[0;32mSwitching to Deploy DIR...\033[0m"
+
+# Start deployment
+DD="$DIR/deploy"
+cd $DD
+
+# Add changes to git.
+git add --all .
+
+# Commit changes.
+msg="Website updates with new changes on `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+echo -e "\033[0;32mDeploying updates to Github...\033[0m"
+
+# Push source and build repos.
+git push -f origin master
+
+echo -e "\033[0;32mChanges successfully published!\033[0m"
